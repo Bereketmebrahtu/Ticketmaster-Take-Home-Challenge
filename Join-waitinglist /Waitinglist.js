cd2e594a1b88bd0b89c3a968bca10390f22ef846
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-11 21:51:29 +0000

    html form with input field and button added as well as css styling for those elemnets

---
+++ Join-waitinglist /Waitinglist.js
@@ -0,0 +1,5 @@
+//  A fan can be notified via email or SMS,
+//  so the form should allow submission of 
+//  email address and/or mobile phone number
+const formEl = document.querySelector('form')
+console.log(formEl)
